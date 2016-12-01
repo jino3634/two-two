390a75dc548b40f3fde5185cc80c127c0b306fbc
DESKTOP-4GHGC44\swpc <tpdud7500@naver.com>
2016-12-01 16:22:20 +0900

    재재수정ㅡㅅ ㅡ

--- pacManGame.c
+++ pacManGame.c
@@ -1536,11 +1536,9 @@ void save(int stage) {
 
 	fpin = fopen("save.txt", "w");
 
-	while (!feof(fpin)) {
-		scanf("%d", &stage);
-		printf("%d", stage);
-		fprintf(fpin, "%d", stage);
-	}
+	fprintf(fpin, "%d", stage);
+
+	printf("스테이지 : %d", stage);
 
 	fclose(fpin);
 }
